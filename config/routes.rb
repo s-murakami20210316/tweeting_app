0d7e5f025ccdcba34b2bcb450b4df53695c241ac
s-murakami20210316 <murashun0120@gmail.com>
2021-04-06 21:48:30 +0900

    投稿一覧、ユーザー一覧ページへのルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,19 @@
 Rails.application.routes.draw do
 
-  get 'users/new'
+  
   # ホームコントローラー、トップページとアバウトページを表示するルーティング
   get '/' => 'home#top'
   get 'about' => 'home#about'
   
   # ポストコントローラー、アクションのルーティング
-  
+  get 'posts/index' => 'posts#index'
+  get 'posts/new' => 'posts#new'
+  post 'posts/create' => 'posts#create'
 
   # ユーザーコントローラー、アクションのルーティング
   get 'login' => 'users#login_form'
   post 'login' => 'users#login'
   get 'signup' => 'users#new'
   post 'signup' => 'users#signup'
+  get 'users/index' => 'users#index'
 end
