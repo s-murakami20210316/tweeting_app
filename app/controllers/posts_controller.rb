64ce1aef4189c6ed722046216ab6d9c9c0fdd707
s-murakami20210316 <murashun0120@gmail.com>
2021-04-06 21:49:38 +0900

    自動生成されたファイル、とりあえずコミット

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,28 @@
+class PostsController < ApplicationController
+  # 投稿一覧アクション
+  def index
+  end
+  
+  # 新規投稿アクション
+  def new
+
+  end
+
+  def create
+
+  end
+
+  # 投稿編集アクション
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  # 投稿削除アクション
+  def destroy
+
+  end
+end
