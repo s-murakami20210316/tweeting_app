99af436d020c3ac28c763511707b7e42542197f5
s-murakami20210316 <murashun0120@gmail.com>
2021-04-06 21:30:00 +0900

    ユーザーコントローラ、サインアップ・ログインページ、それぞれのアクション作成とルーティングを設定

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,19 @@
+class UsersController < ApplicationController
+
+  # サインアップ関連のアクション
+  def new
+  end
+
+  def create
+
+  end
+  
+  # ログイン関連のアクション
+  def login_form
+
+  end
+
+  def login
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,16 @@
 Rails.application.routes.draw do
 
+  get 'users/new'
   # ホームコントローラー、トップページとアバウトページを表示するルーティング
   get '/' => 'home#top'
   get 'about' => 'home#about'
+  
+  # ポストコントローラー、アクションのルーティング
+  
 
+  # ユーザーコントローラー、アクションのルーティング
+  get 'login' => 'users#login_form'
+  post 'login' => 'users#login'
+  get 'signup' => 'users#new'
+  post 'signup' => 'users#signup'
 end
